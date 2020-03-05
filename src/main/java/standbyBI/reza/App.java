4538407dec6ebabf785b17958f3431707e54966a
Fahrul Arya Sugita <faryasugita@gmail.com>
2020-03-06 01:35:53 +0700

    standby BI g main main

---
+++ src/main/java/standbyBI/reza/App.java
@@ -0,0 +1,13 @@
+package standbyBI.reza;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "sangat tidak main dalam stand by BI" );
+    }
+}
