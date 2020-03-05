54adcb7ffb6299f3bba33e741596b9b0fbb562c4
Fahrul Arya Sugita <faryasugita@gmail.com>
2020-03-06 01:59:11 +0700

    ok add

--- src/main/java/standbyBI/reza/App.java
+++ src/main/java/standbyBI/reza/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "sangat tidak main dalam stand by BI" );
+        System.out.println("Yuk masri dalam kehidupan yang fana");
     }
 }
