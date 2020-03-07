12f4f19fb50d9e2418e4cc72da0cd986c61361a3
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2020-03-07 21:54:10 +0700

    Update App.java

--- src/main/java/standbyBI/reza/App.java
+++ src/main/java/standbyBI/reza/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "sangat tidak main dalam stand by BI" );
+        System.out.println( "Sudah Sangat tidak main2" );
     }
 }
